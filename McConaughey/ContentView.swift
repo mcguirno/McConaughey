4443657c532008ffdd0f2d793c21b9634f8c6317
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-02-17 18:09:27 -0500

    Exercise from 2/17 Class

--- McConaughey/ContentView.swift
+++ McConaughey/ContentView.swift
@@ -6,16 +6,52 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var stepperValue = 1
+    @State private var audioPlayer: AVAudioPlayer!
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("How is Matt?")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            Image("matt")
+                .resizable()
+                .scaledToFit()
+                .clipShape(.rect(cornerRadius: 30))
+                .shadow(radius: 10)
+            
+            Spacer()
+            
+            Stepper("How Alright is Matt?", value: $stepperValue, in: 1...10)
+            Text(String(stepperValue))
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            Button("Tell Me!") {
+                playSound(soundName:"alright")
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
+    }
+    func playSound(soundName: String) {
+        if audioPlayer != nil && audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("Could not read file named \(soundName)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.numberOfLoops = stepperValue - 1
+            audioPlayer.play()
+        } catch {
+            print("ERROR: \(error.localizedDescription)")
+        }
     }
 }
 
