aa15b4c930690120a18334f75ffa7fd4f26d9d42
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-02-17 17:46:03 -0500

    Initial Commit

---
+++ McConaughey/McConaugheyApp.swift
@@ -0,0 +1,17 @@
+//
+//  McConaugheyApp.swift
+//  McConaughey
+//
+//  Created by Noah McGuire on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct McConaugheyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
